cacb05b7ab9e8ec6f451004ab3b7e2640bb93283
Mahmudul Hasan <35046361+Hasan-75@users.noreply.github.com>
2019-02-23 22:36:58 +0600

    Update firebase_crud.py

--- Mysite/music/others/firebase_crud.py
+++ Mysite/music/others/firebase_crud.py
@@ -4,21 +4,10 @@ from pyrebase import pyrebase
 
 class FB_C:
     config = {
-        'apiKey': "AIzaSyBnmaZaBEOclGeVeJHygiTMvSvc6v2BWMg",
-        'authDomain': "theoryofcreativity-4241e.firebaseapp.com",
-        'databaseURL': "https://theoryofcreativity-4241e.firebaseio.com",
-        'projectId': "theoryofcreativity-4241e",
-        'storageBucket': "theoryofcreativity-4241e.appspot.com",
-        'messagingSenderId': "789207444088",
+        #firebase configs here
     }
     config_a = {
-        'apiKey': "AIzaSyBnmaZaBEOclGeVeJHygiTMvSvc6v2BWMg",
-        'authDomain': "theoryofcreativity-4241e.firebaseapp.com",
-        'databaseURL': "https://theoryofcreativity-4241e.firebaseio.com",
-        'projectId': "theoryofcreativity-4241e",
-        'storageBucket': "theoryofcreativity-4241e.appspot.com",
-        'messagingSenderId': "789207444088",
-        "serviceAccount": 'theoryofcreativity-4241e-firebase-adminsdk-94vcb-f88eb91eb9.json'
+        #firebase admin configs here
     }
 
     def get_firebase_instance(self, **kwargs):
